b2bcdd3ab0e73c6f8f29b99b7bfbf07f6747e271
sarwan239 <sarwan.raja@gmail.com>
2020-10-07 17:18:27 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.employee_hub;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("EmployeeInfoDB");
+            services.AddDbContextPool<employee_hubContext>(options => options.UseMySQL(connection));
+            services.AddControllers();
+            //services.Configure<IISServerOptions>(options =>
+            //{
+            //    options.AutomaticAuthentication = false;
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,76 @@
+﻿using EmployeeManagement.employee_hub;
+using EmployeeManagement.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public static class Utilities
+    {
+        public static IConfiguration configuration { get; set; }
+
+        /// <summary>
+        /// Function to send Welcome email notification to the cocerned employee
+        /// </summary>
+        /// <param name="employeeInfo"></param>
+        /// <returns></returns>
+        public static bool SendWelcomeMail(EmployeeInfoDTO employeeInfo)
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Function to send delete email notification to the concerned employee
+        /// </summary>
+        /// <param name="employeeInfo"></param>
+        /// <returns></returns>
+        public static bool SendDeleteNotification(EmployeeInfoDTO employeeInfo)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Validates an input email id and returns true if valid and false in case of invalid email id.
+        /// </summary>
+        /// <param name="emailaddress"></param>
+        /// <returns>boolean value</returns>
+        public static bool IsValidEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress omailAddress = new MailAddress(emailaddress);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Function to write logs
+        /// </summary>
+        /// <param name="message"></param>
+        public static void WriteLog(string message)
+        {
+            var logfile = configuration["LogFileName"];
+            if (!string.IsNullOrEmpty(logfile))
+            {
+                var processId = Process.GetCurrentProcess().Id;
+                logfile = logfile.Replace(".txt", "_" + processId + "_" + DateTime.Now.ToString("yyyyMMddHH") + ".txt");
+                TextWriter logWriter = new StreamWriter(logfile, true);
+                logWriter.WriteLine("{0} : {1}", DateTime.Now, message);
+                logWriter.Flush();
+                logWriter.Close();
+            }
+        }
+
+    }
+}
