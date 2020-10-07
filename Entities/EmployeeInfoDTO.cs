304b1490616dc3c27b6e39da0415719d76452a54
sarwan239 <sarwan.raja@gmail.com>
2020-10-07 17:24:37 +0530

    Add files via upload

---
+++ Entities/EmployeeInfoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Entities
+{
+    public class EmployeeInfoDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string DepartmentName { get; set; }
+        public string ManagerName { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
